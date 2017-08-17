9d8d57b89fc9d8796b44c9706a31effd3b1f522c
mpppk <niboshiporipori@gmail.com>
2017-08-17 22:45:53 +0900

    Convert to CLI project

--- src/app.ts
+++
@@ -1,4 +0,0 @@
-declare const h5: any;
-
-import rootController from './controller/RootController';
-h5.core.controller('#container', rootController );
--- src/controller/IController.ts
+++
@@ -1,6 +0,0 @@
-export interface IController {
-    __name: string;
-    __construct?: (context: any) => void;
-    __init?: (context: any) => void;
-    __ready?: (context: any) => void;
-}
